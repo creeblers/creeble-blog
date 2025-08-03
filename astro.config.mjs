95a88c61e107480ae86b77e4f3c29f16e3405fd1
Jean-Christio Martin <admin@marjc.ch>
2025-08-03 20:00:24 +0200

    chore: Configure site URL and base path for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
+    site: 'https://creeblers.github.io',
+    base: '/creeble-blog',
     server: {
         host: '0.0.0.0',
         port: 4321,
