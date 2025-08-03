2ee1be3c3d910ed7d59f390812d159fe4869abff
Jean-Christio Martin <admin@marjc.ch>
2025-08-03 20:54:11 +0200

    chore: Update site URL and base path for development and production environments, add Prism.js for syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
-    site: 'https://creeblers.github.io',
-    base: '/creeble-blog',
+    site: import.meta.env.DEV ? 'https://creeble-blog.localhost' : 'https://creeblers.github.io',
+    base: import.meta.env.DEV ? '/' : '/creeble-blog',
     server: {
         host: '0.0.0.0',
         port: 4321,
